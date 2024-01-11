5167f01e50edc9bf1727ae9d9d4b455722abe654
eunsungshin <eunsung.shin@gmail.com>
2024-01-11 10:29:24 +0900

    streamlit ui 추가;버튼 비활성화 오류 있음

--- streamlit/ui.py
+++ streamlit/ui.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import requests
+from dotenv import load_dotenv
+import os
 
-
-
+load_dotenv()
+default_openai_api_key = os.environ['OPENAI_API_KEY']
 
 st.title('유튜브 요약 서비스:sunglasses:')
 
@@ -11,15 +13,31 @@ def request_summary(url : str, api_key : str) :
     response = requests.post(f'http://localhost:8000/summary', json = {'url' : url, 'api_key' : api_key})
     return response.json()
 
+def disable() :
+    st.session_state.disabled = True
 
-with st.form('my_form') :
-    summary_result = ''
-    # with input_form :
-    url = st.text_input("요약이 필요한 Youtube URL을 입력해주세요.", key = 'url')
-    api_key = st.text_input("OpenAI api key를 입력해주세요. [해당 링크](https://platform.openai.com/api-keys)에서 만들 수 있습니다.", key = 'api_key')
-    # with submit_button :
-    submitted = st.form_submit_button('요약')
+if 'disabled' not in st.session_state :
+    st.session_state.disabled = False
+
+summary_result = ''
+with st.form('my-form') :
+    url = st.text_input("요약이 필요한 Youtube URL을 입력해주세요.", 
+                        key = 'url',
+                        )
+    api_key = st.text_input("OpenAI api key를 입력해주세요. [해당 링크](https://platform.openai.com/api-keys)에서 만들 수 있습니다.", 
+                            key = 'api_key',
+                            autocomplete=default_openai_api_key,
+                            )
+
+    submitted = st.form_submit_button('요약', on_click = disable, disabled = st.session_state.disabled)
+
     if submitted :
-        summary_result = request_summary(url = url, api_key = api_key)
+        summary_result = request_summary(url, api_key)
+        st.session_state.disabled = False
         
         st.write(summary_result)
+
+
+
+
+
